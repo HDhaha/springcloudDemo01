8e20d662a7ca07e39377d5f2ce749efdacd6a630
HD <18539479896@163.com>
2023-04-14 16:20:19 +0800

    测试合并,第一次提交

---
+++ cloud-consumer-zookeeper-order80/src/main/java/com/springcloud/controller/ConsumerPaymentController.java
@@ -0,0 +1,27 @@
+package com.springcloud.controller;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
+
+/**
+ * @author 申浩东
+ */
+@Slf4j
+@RestController
+@RequestMapping(value = "/consumer/payment")
+public class ConsumerPaymentController {
+//    private final static String PAYMENT_URL="http://localhost:8001/payment";
+    //cloud-provider-payment
+    private final static String PAYMENT_URL="http://payment-service";
+    @Autowired
+    private RestTemplate restTemplate;
+    @GetMapping(value = "/zk")
+    public String getPayment(){
+        return restTemplate.getForObject(PAYMENT_URL+"/payment/zk",String.class);
+    }
+
+}
